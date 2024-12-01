5fc2783dd2ebea2a2fba72c61ebfe04cbce6ae3e
Niccolo Cavallini <ncavallini@ethz.ch>
2024-12-01 14:33:59 +0100

    Fixed delete bug

--- actions/groups/delete.php
+++ actions/groups/delete.php
@@ -10,4 +10,12 @@ $group_id = $_GET['id'];
 $sql = "DELETE FROM groups WHERE group_id = ?";
 $stmt = $connection->prepare($sql);
 $stmt->execute([$group_id]);
+
+$sql = "DELETE FROM group_tasks WHERE group_id = ?";
+$stmt = $connection->prepare($sql);
+$stmt->execute([$group_id]);
+
+$sql = "DELETE FROM group_coins WHERE group_id = ?";
+$stmt = $connection->prepare($sql);
+$stmt->execute([$group_id]);
 header("Location: ../../index.php?page=manage");
