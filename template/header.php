ff8e8942d918c761d190591d236ae01653f9c0c9
Niccolo Cavallini <ncavallini@ethz.ch>
2024-12-04 10:52:07 +0100

    Updated header

--- template/header.php
+++ template/header.php
@@ -6,6 +6,13 @@
 
     <link rel="stylesheet" href="inc/styles.css">
 
+    <link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="favicon.svg" />
+<link rel="shortcut icon" href="favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="Visualizing Mental Load" />
+<link rel="manifest" href="site.webmanifest" />
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <script src="https://kit.fontawesome.com/f5ed638bc2.js" crossorigin="anonymous"></script>
